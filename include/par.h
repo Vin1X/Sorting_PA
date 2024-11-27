865c141c7230eb13cb7778f1808df346f7ba194b
Arian <hartlarian@gmail.com>
2024-11-27 10:40:14 +0100

    implement parallel sorting

---
+++ include/par.h
@@ -0,0 +1,14 @@
+#ifndef PAR_H
+#define PAR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pthread.h>
+#include "com.h"
+
+#define LOG_FILE "par.log"
+
+int main();
+
+#endif
